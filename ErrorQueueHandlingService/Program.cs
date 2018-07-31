ec55dc67510dfc8e4eeb6013394ca1ab8d7299cf
Bhargav Mehta <bhargav.mehta@wolterskluwer.com>
2018-07-31 16:25:27 +0530

    ErrorQueueServiceAdded

---
+++ ErrorQueueHandlingService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorQueueHandlingService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Error Queue Handling Service.");
+			//Console.WriteLine("Second change in Error Queue Handling Service.");
+			//Console.WriteLine("Third change in Error Queue Handling Service.");
+			Console.ReadLine();
+		}
+	}
+}
